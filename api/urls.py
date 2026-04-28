c0456bacf04db9a5017b4c1eff7d1288a7d7888d
Rohit Thete <rohitthete.512@gmail.com>
2026-04-28 11:23:12 +0530

    made get and put views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
-from .views import register
+from .views import register,login_user,UserView
 from django.urls import path
 
 urlpatterns = [
-    path('register/',register)
+    path('register/',register),
+    path('login/',login_user),
+    path('user/',UserView.as_view()),
+
 ]
